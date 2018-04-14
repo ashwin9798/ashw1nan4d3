09b600887cf57aa3babcb7750f145e950646ed49
dkok97 <dinkarkhattar@gmail.com>
2018-04-14 04:07:12 -0700

    extra error handling

--- Project 1A/calculator.php
+++ Project 1A/calculator.php
@@ -4,26 +4,38 @@
 
 <h1>Calculator - CS 143 Project 1A</h1>
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
   Calculate: <input type="text" name="expression">
   <input type="submit">
 </form>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     // collect value of input field
+    if (!array_key_exists('expression', $_REQUEST)) {
+      return;
+    }
     $name = $_REQUEST['expression'];
-    if (empty($name)) {
-        echo "expression is empty";
+    $name_noSpace = str_replace(' ', '', $name);
+    if (empty($name_noSpace)) {
+        echo "<h2> Result </h2>";
+        echo "Expression is empty";
+        return;
+    } elseif (preg_match('/((\-*)(\d+)([\+\-\*\/]*)+)/', $name_noSpace, $matches)==false) {
+        echo "<h2> Result </h2>";
+        echo "Invalid Expression!";
+        return;
+    } elseif (strpos($name_noSpace, '/0')!==false){
+      echo "<h2> Result </h2>";
+      echo "Division by zero. Error!";
+      return;
     } else {
         ob_start();
-        eval( '$result = (' . $name . ');' );
-        ob_start();
-        if ('' !== $error = ob_get_clean()) {
-            echo "akjsdhkajs";
-        }
-
-        echo $result;
+        eval( '$result = (' . $name_noSpace . ');' );
+        echo "<h2> Result </h2>";
+        echo "$name : $result";
     }
 }
 ?>
