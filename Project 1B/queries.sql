9ec34eef02259f6eca8f75ebdf197a92213eaf59
Ashwin Vivek <ashwin9798@gmail.com>
2018-04-15 14:33:21 -0700

    finish web query interface

--- Project 1B/queries.sql
+++ Project 1B/queries.sql
@@ -1,8 +1,8 @@
-SELECT DISTINCT CONCAT(actor.first, ' ', actor.last)
+SELECT DISTINCT CONCAT(actor.first, ' ', actor.last) as DieAnotherDayActors
 FROM Actor actor, Movie movie, MovieActor movie_actor
 WHERE movie.title='Die Another Day' AND movie.id=movie_actor.mid AND movie_actor.aid=actor.id;
 
-SELECT COUNT(DISTINCT movie_actor.aid)
+SELECT COUNT(DISTINCT movie_actor.aid) as MultipleMovieActors
 FROM MovieActor movie_actor, MovieActor movie_actor2
 WHERE movie_actor.aid=movie_actor2.aid AND movie_actor.mid<>movie_actor2.mid;
 
